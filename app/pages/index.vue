7ee4e82dd7e1c3162f287870798cf3d5668c7e2b
Jeff 'LunchBox' Boshers <boundbystars@gmail.com>
2025-10-20 12:35:45 -0500

    Add Index Page and brand theme css

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="grid grid-flow-col md:grid-flow-row">
+    <div class="spacer">
+      <h1>Welcome Back</h1>
+    </div>
+    <div></div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/test-utils',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxt/fonts"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: {
+    families: [{ name: "Manrope", weights: [400, 600, 700] }],
+  },
+});
